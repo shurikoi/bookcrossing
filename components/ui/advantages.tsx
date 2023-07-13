df92f6a0de51dab05ab2a5bc63528bea3a9a064e
Vladyslav Verbovetskyi <verbovetskiy.v@gmail.com>
2023-07-13 20:11:26 +0200

    some changes

--- components/ui/advantages.tsx
+++ components/ui/advantages.tsx
@@ -1,90 +1,53 @@
 import Image from "next/image";
-import { helveticaNeueCyr } from "./fonts";
-
+import { eUkraine } from "./fonts";
 
 export default function Advantages() {
     return (
-        <div className="px-[70px] 2md:px-[150px] py-[75px] font-medium text-xl">
+        <div className="flex flex-col items-center px-[70px] 2md:px-[230px] py-[75px] font-medium text-2xl">
             <div className="text-center">
-                Wierzymy, że korzystanie z serwisu może dostarczyć <br /> wiele
-                radości i wzbogacić Twoje doświadczenie czytelnicze.
+                Wierzymy, że korzystanie z serwisu może dostarczyć <br /> wiele radości i wzbogacić Twoje doświadczenie
+                czytelnicze.
             </div>
-            <div className="grid grid-cols-1 gap-36 gap-y-20 mt-[110px] 2md:grid-cols-2 2md:text-left text-center">
-                <div className="flex flex-col gap-4 flex-1 items-center 2md:items-start">
+            <div className="inline-grid grid-cols-1 gap-12 gap-x-24 mt-[110px] 2md:grid-cols-[auto_auto] 2md:text-left text-center">
+                <div className="max-w-[600px] flex flex-col justify-center gap-4 flex-1 items-center 2md:items-start">
                     <div>
-                        <Image
-                            width={90}
-                            height={90}
-                            src="/images/image 1553.png"
-                            alt=""
-                        ></Image>
+                        <Image width={90} height={90} src="/images/image 1553.png" alt=""></Image>
                     </div>
-                    <div
-                        className={`font-semibold ${helveticaNeueCyr.className}`}
-                    >
-                        Zrównoważony styl życia
-                    </div>
-                    <div className="text-sm">
-                        Tutaj skorzystając z już istniejących książek i dzieląc
-                        się nimi z innymi, przyczyniasz się do redukcji
-                        marnotractwa i zrównoważonego stylu życia.
+                    <div className={`font-semibold ${eUkraine.className}`}>Zrównoważony styl życia</div>
+                    <div className="text-base w-fit">
+                        Tutaj skorzystając z już istniejących książek i dzieląc się nimi z innymi, przyczyniasz się do
+                        redukcji marnotractwa i zrównoważonego stylu życia.
                     </div>
                 </div>
-                <div className="flex flex-col gap-4 flex-1 items-center 2md:items-start">
+
+                <div className="max-w-[600px] flex flex-col gap-4 flex-1 items-center 2md:items-start">
                     <div>
-                        <Image
-                            width={90}
-                            height={90}
-                            src="/images/image 359.png"
-                            alt=""
-                        ></Image>
+                        <Image width={90} height={90} src="/images/image 359.png" alt=""></Image>
                     </div>
-                    <div
-                        className={`font-semibold ${helveticaNeueCyr.className}`}
-                    >
-                        Wymiana
-                    </div>
-                    <div className="text-sm">
-                        To doskonały sposób na pozbycie się niechcianych książek
-                        i zdobycie nowych, bez konieczności kupowania.
+                    <div className={`font-semibold ${eUkraine.className}`}>Wymiana</div>
+                    <div className="text-base">
+                        To doskonały sposób na pozbycie się niechcianych książek i zdobycie nowych, bez konieczności
+                        kupowania.
                     </div>
                 </div>
-                <div className="flex flex-col gap-4 flex-1 items-center 2md:items-start">
+
+                <div className="max-w-[600px] flex flex-col gap-4 flex-1 items-center 2md:items-start">
                     <div>
-                        <Image
-                            width={90}
-                            height={90}
-                            src="/images/image 294.png"
-                            alt=""
-                        ></Image>
+                        <Image width={90} height={90} src="/images/image 294.png" alt=""></Image>
                     </div>
-                    <div
-                        className={`font-semibold ${helveticaNeueCyr.className}`}
-                    >
-                        Odkrywanie nowych książek
-                    </div>
-                    <div className="text-sm">
-                        Umożliwiamy odkrywanie nowych tytułów i autorów, których
-                        wcześniej nie znałeś.
+                    <div className={`font-semibold ${eUkraine.className}`}>Odkrywanie nowych książek</div>
+                    <div className="text-base">
+                        Umożliwiamy odkrywanie nowych tytułów i autorów, których wcześniej nie znałeś.
                     </div>
                 </div>
-                <div className="flex flex-col gap-4 flex-1 items-center 2md:items-start">
+
+                <div className="max-w-[600px] flex flex-col gap-4 flex-1 items-center 2md:items-start">
                     <div>
-                        <Image
-                            width={90}
-                            height={90}
-                            src="/images/image 1016.png"
-                            alt=""
-                        ></Image>
+                        <Image width={90} height={90} src="/images/image 1016.png" alt=""></Image>
                     </div>
-                    <div
-                        className={`font-semibold ${helveticaNeueCyr.className}`}
-                    >
-                        Za darmo
-                    </div>
-                    <div className="text-sm">
-                        BookCrossing oferuje możliwość czytania i odkrywania
-                        nowych książek bez żadnych kosztów.
+                    <div className={`font-semibold ${eUkraine.className}`}>Za darmo</div>
+                    <div className="text-base">
+                        BookCrossing oferuje możliwość czytania i odkrywania nowych książek bez żadnych kosztów.
                     </div>
                 </div>
             </div>
--- components/ui/getStarted.tsx
+++ components/ui/getStarted.tsx
@@ -1,30 +1,25 @@
 import Image from "next/image";
-import { helveticaNeueCyr } from "./fonts";
+import { eUkraine} from "./fonts";
 
 export default function GetStarted() {
     return (
-            <div className="flex flex-col items-center gap-[110px] px-[70px] 2md:px-[150px] py-[75px] font-medium text-xl">
+            <div className="flex flex-col items-center gap-[110px] px-[70px] 2md:px-[230px] py-[75px] font-medium text-2xl">
                 <div className="text-center">Zobacz jak zacząć</div>
-                <div className="grid grid-cols-1 gap-36 gap-y-20 2md:grid-cols-2 2md:text-left text-center">
-                    <div className="flex flex-col gap-4 flex-1 items-center 2md:items-start">
-                        <div>
-                            <Image width={90} height={90} src="/images/phone.png" alt=""></Image>
-                        </div>
-                        <div className={`font-semibold ${helveticaNeueCyr.className}`}>Wejdź do systemu</div>
-                        <div className="text-sm">Ciśnij ‘Get Started’.</div>
-                    </div>
-                    <div className="flex flex-col gap-4 flex-1 items-center 2md:items-start">
+                <div className="inline-grid grid-cols-1 gap-12 gap-x-24 mt-[110px] 2md:grid-cols-[auto_auto] 2md:text-left text-center">
+                    
+                    <div className="max-w-[600px] flex  flex-col justify-center gap-4 flex-1 items-center 2md:items-start">
                         <div>
                             <Image width={90} height={90} src="/images/image 359.png" alt=""></Image>
                         </div>
-                        <div className={`font-semibold ${helveticaNeueCyr.className}`}>Oddajesz i dostajesz</div>
-                        <div className="text-sm">
+                        <div className={`font-semibold ${eUkraine.className}`}>Oddajesz i dostajesz</div>
+                        <div className="text-base">
                             Kiedy twoja książka znajdzie nowego właścicela dostajesz 1 point.
                             <br />
                             <br />
                             Jeden point wymienisz na jedną książkę.
                         </div>
                     </div>
+                    
                 </div>
                 <div className="group">
                     <div className="bg-[#00FF0A] w-fit rounded-2xl px-7 py-2.5 font-semibold transition duration-200 group-hover:translate-y-[-3px] cursor-pointer">
