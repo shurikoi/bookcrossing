a6570278bba5153c4dc22172c4f6f999289819e8
Vladyslav Verbovetskyi <verbovetskiy.v@gmail.com>
2023-07-15 02:58:01 +0200

    added form bg black/50

--- components/ui/SignInForm.tsx
+++ components/ui/SignInForm.tsx
@@ -25,26 +25,30 @@ export default function signInForm() {
     }, [formActive, formRef]);
 
     return (
-        <form
-            ref={formRef}
-            className={`fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] flex flex-col gap-9 max-w-[460px] px-14 py-10 bg-white shadow-sm text-center duration-300 rounded-lg ${
+        <div
+            className={`flex items-center justify-center fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]  w-screen h-screen bg-black/50  duration-300 ${
                 formActive ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
             }`}
         >
-            <CrossBtn></CrossBtn>
-            <div className="text-2xl font-semibold">Zaloguj się lub zarejestruj w kilka sekund</div>
-            <div className="text-[13px] font-extralight">
-                Użyj adresu e-mail. Sprawdzimy, czy już masz konto. Jeśli nie, pomożemy Ci je utworzyć.
-            </div>
-            <div className="border-[#61C558] border-2 rounded-lg px-4 py-2.5 text-[15px] flex justify-between">
-                <input type="text" placeholder="myemail@example.com" className="outline-none" />
-                <ArrowBtn></ArrowBtn>
-            </div>
-            <div className="font-medium text-[15px]">Lub</div>
-            <div className="flex gap-6 px-5 py-3 items-center bg-[#EFEFEF] rounded-lg">
-                <Image alt="" src="/images/google.png" width={25} height={25}></Image>
-                <div className="font-extralight cursor-pointer">Kontynuuj przez Google</div>
-            </div>
-        </form>
+            <form
+                ref={formRef}
+                className={`relative flex flex-col gap-9 max-w-[460px] px-14 py-10 bg-white shadow-sm text-center rounded-lg `}
+            >
+                <CrossBtn></CrossBtn>
+                <div className="text-2xl font-semibold">Zaloguj się lub zarejestruj w kilka sekund</div>
+                <div className="text-[13px] font-extralight">
+                    Użyj adresu e-mail. Sprawdzimy, czy już masz konto. Jeśli nie, pomożemy Ci je utworzyć.
+                </div>
+                <div className="border-[#61C558] border-2 rounded-lg px-4 py-2.5 text-[15px] flex justify-between">
+                    <input type="text" placeholder="myemail@example.com" className="outline-none" />
+                    <ArrowBtn></ArrowBtn>
+                </div>
+                <div className="font-medium text-[15px]">Lub</div>
+                <div className="flex gap-6 px-5 py-3 items-center bg-[#EFEFEF] rounded-lg">
+                    <Image alt="" src="/images/google.png" width={25} height={25}></Image>
+                    <div className="font-extralight cursor-pointer">Kontynuuj przez Google</div>
+                </div>
+            </form>
+        </div>
     );
 }
