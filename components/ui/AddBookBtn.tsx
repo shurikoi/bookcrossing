8d49ab0ba7d92836bca80f8cdea2b890d2cde24f
Vladyslav Verbovetskyi <verbovetskiy.v@gmail.com>
2023-07-23 01:43:27 +0200

    some changes

--- components/ui/AddBookBtn.tsx
+++ components/ui/AddBookBtn.tsx
@@ -3,7 +3,7 @@ import Pencil from "./Pencil";
 export default function AddBookBtn() {
     return (
         <div className="group">
-            <div className="shadow-md px-6 py-2.5 right-5 bottom-5 flex items-center bg-[#F2F9F0] gap-3 fixed rounded-lg duration-300 group-hover:-translate-y-1 cursor-pointer">
+            <div className="shadow-md px-6 py-2.5 right-5 bottom-5 flex items-center bg-[#F2F9F0] gap-3 fixed rounded-lg duration-200 group-hover:-translate-y-1 cursor-pointer will-change-transform">
                 <Pencil />
                 <div className="font-normal">Utwórz</div>
             </div>
