caae1f4aa5e2cc1c878a989de953d86dcae32552
Vladyslav Verbovetskyi <verbovetskiy.v@gmail.com>
2023-08-27 16:17:22 +0200

    some changes

--- app/api/getUserData/route.ts
+++ app/api/getUserData/route.ts
@@ -1,10 +1,13 @@
+import connection from "@/lib/connection";
 import users from "@/model/user";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     const body = (await req.json()) as { email: string };
-
+    
     const { email } = body;
+
+    await connection()
 
     const { name, surname, points, password } = await users.findOne({ email });
 
--- components/contexts/UserProviders.tsx
+++ components/contexts/UserProviders.tsx
@@ -36,6 +36,7 @@ function UserProvider({ children }: { children: React.ReactNode }) {
 
     useEffect(() => {
         async function fetchUser() {
+            console.log(userEmail)
             const response = await fetch("/api/getUserData", {
                 method: "post",
                 body: JSON.stringify({ email: userEmail }),
