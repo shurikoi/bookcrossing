55006ac138444996458250fa2a54d10aac61ff4c
Vladyslav Verbovetskyi <verbovetskiy.v@gmail.com>
2023-10-24 23:40:35 +0200

    some changes

--- app/api/get-publication/route.ts
+++ app/api/get-publication/route.ts
@@ -14,11 +14,15 @@ export async function POST(req: Request) {
 
     const book = await books.findOne({ _id: id }, { _id: 0 });
     const owner = await users.findOne({ _id: book?.owner }, { avatar: 1, name: 1, surname: 1 });
-    console.log(book.reservedBy);
-    if (book.reservedBy && session?.user?.id == book?.owner.toString()) {
-        const reservator = await users.findOne({ _id: book.reservedBy }, { name: 1, surname: 1 });
 
-        return NextResponse.json({ ...book._doc, ownerData: owner._doc, reservatorData: reservator._doc });
+    if (book.reservedBy) {
+        if (session?.user?.id == book?.owner.toString()) {
+            const reservator = await users.findOne({ _id: book.reservedBy }, { name: 1, surname: 1 });
+
+            return NextResponse.json({ ...book._doc, ownerData: owner._doc, reservatorData: reservator._doc });
+        } else if (book.reserveBy.toString() == session?.user?.id) {
+            return NextResponse.json({ ...book._doc, ownerData: owner._doc, amIReservator: true});
+        }
     }
 
     return NextResponse.json({ ...book._doc, ownerData: owner._doc });
