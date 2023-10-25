3e31784ca8f051a62360fd1a878aa8c83a4d03ba
Oleksandr Bezverkhnii <bezverkhniispace@gmail.com>
2023-10-25 09:19:17 +0200

    fixed get publ err

--- app/api/get-publication/route.ts
+++ app/api/get-publication/route.ts
@@ -11,19 +11,29 @@ export async function POST(req: Request) {
     await connection();
 
     const session = await getServerSession(authOptions);
-
+    console.log(id)
     const book = await books.findOne({ _id: id }, { _id: 0 });
+    console.log(book)
     const owner = await users.findOne({ _id: book?.owner }, { avatar: 1, name: 1, surname: 1 });
 
-    if (book._doc.reservedBy) {
+    if (book?.reservedBy) {
         if (session?.user?.id == book?.owner.toString()) {
             const reservator = await users.findOne({ _id: book.reservedBy }, { name: 1, surname: 1 });
 
-            return NextResponse.json({ ...book._doc, ownerData: owner._doc, reservatorData: reservator._doc });
-        } else if (book.reserveBy._doc.toString() == session?.user?.id) {
-            return NextResponse.json({ ...book._doc, ownerData: owner._doc, amIReservator: true});
+            return NextResponse.json({ ...book?._doc, ownerData: owner?._doc, reservatorData: reservator?._doc });
+        } else if (book?.reservedBy.toString() == session?.user?.id) {
+            return NextResponse.json({ ...book?._doc, ownerData: owner?._doc, amIReservator: true});
         }
     }
 
-    return NextResponse.json({ ...book._doc, ownerData: owner._doc });
+    return NextResponse.json({ ...book?._doc, ownerData: owner?._doc });
 }
+
+
+
+
+
+
+
+
+
